8a007eb8ab3be5f357adef7c7f4d853c8aaa1bbc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-05-10 15:56:17 +0900

    add polymorphic function samples

---
+++ samples/07_polymorphic/devide0.hs
@@ -0,0 +1,3 @@
+devide :: Double -> Double -> Double
+devide 0 b = b
+devide a b = b / a
--- samples/07_polymorphic/polymorphic.hs
+++ samples/07_polymorphic/polymorphic.hs
@@ -5,3 +5,6 @@ myFromMaybe d _ = d
 myMaybe :: b -> (a -> b) -> Maybe a -> b
 myMaybe _ f (Just x) = f x
 myMaybe d _ _ = d
+
+myId :: a -> a
+myId x = x
