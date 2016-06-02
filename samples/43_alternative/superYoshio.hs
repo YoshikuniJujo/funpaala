8348e2d1522bda70c9bd828d97d72a9dde286e71
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-06-02 10:02:32 +0900

    add sample code

--- samples/43_alternative/superYoshio.hs
+++ samples/43_alternative/superYoshio.hs
@@ -22,4 +22,45 @@ stage1, stage2 :: [Event]
 stage1 = [E Ghost, I Shell, I Star, I Coin, E Troll, I Coin, E Vampire]
 stage2 = [E Vampire, E Vampire, I Star, I Star, I Star, I Coin, I Star]
 
--- mguard :: Boool -> Maybe
+mguard :: Bool -> Maybe ()
+mguard True = Just ()
+mguard _ = Nothing
+
+mevent :: Yoshio -> Event -> Maybe Yoshio
+mevent (hp, s) (E e) = do
+	let hp' = hp - damage e
+	mguard $ hp' > 0
+	return (hp', s)
+mevent (hp, s) (I i) = return (hp, s + score i)
+
+mgame :: Yoshio -> [Event] -> Maybe Yoshio
+mgame = foldM mevent
+
+malt :: Maybe a -> Maybe a -> Maybe a
+mx@(Just _) `malt` _ = mx
+_ `malt` my = my
+
+malts :: [Maybe a] -> Maybe a
+malts (x : xs) = x `malt` malts xs
+malts _ = Nothing
+
+lguard :: Bool -> [()]
+lguard True = [()]
+lguard _ = []
+
+levent :: Yoshio -> Event -> [Yoshio]
+levent (hp, s) (E e) = do
+	let hp' = hp - damage e
+	lguard $ hp' > 0
+	return (hp', s)
+levent (hp, s) (I i) = return (hp, s + score i)
+
+lgame :: Yoshio -> [Event] -> [Yoshio]
+lgame = foldM levent
+
+lalt :: [a] -> [a] -> [a]
+lalt = (++)
+
+lalts :: [[a]] -> [a]
+lalts (x : xs) = x `lalt` lalts xs
+lalts _ = []
