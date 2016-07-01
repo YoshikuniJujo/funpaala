15f79f507e66bd0abb158e8b80c5483e16c58a61
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-07-01 13:44:18 +0900

    correct sample function estPi4

--- samples/13_recursion/leibniz.hs
+++ samples/13_recursion/leibniz.hs
@@ -1,6 +1,10 @@
 estPi :: Int -> Double
--- estPi n = 4 * estPi4 (fromIntegral n)
-estPi = (4 *) . estPi4 . fromIntegral
+-- estPi n = 4 * estPi4 n
+estPi = (4 *) . estPi4
 
+estPi4 :: Int -> Double
 estPi4 0 = 1
-estPi4 n = estPi4 (n - 1) + (- 1) ** n / (2 * n + 1)
+estPi4 n = estPi4 (n - 1) + (- 1) ^ n / (2 * fromIntegral n + 1)
+
+estPi' :: Int -> Double
+estPi' n = (4 *) . sum . take n $ map (\k -> (- 1) ** k / (2 * k + 1)) [0 ..]
