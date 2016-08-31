6ffd32da6d19e851edd41de94bfeec19ab4679bd
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-08-31 09:59:08 +0900

    11. montecarlo: don't use module Arrow

--- samples/11_montecarlo/montecarlo.hs
+++ samples/11_montecarlo/montecarlo.hs
@@ -1,9 +1,11 @@
-import Control.Arrow
 import System.Random
 
+randomXYs :: Int -> ([Double], [Double])
+randomXYs n = let (g, g') = split $ mkStdGen n in
+	(randomRs (-1, 1) g, randomRs (-1, 1) g')
+
 points :: Int -> [(Double, Double)]
-points =
-	uncurry zip . (randomRs (-1, 1) *** randomRs (-1, 1)) . split . mkStdGen
+points n = let (xs, ys) = randomXYs n in zip xs ys
 
 inCircle :: (Double, Double) -> Bool
 inCircle (x, y) = x ^ 2 + y ^ 2 <= 1
