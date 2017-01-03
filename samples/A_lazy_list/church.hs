e77dd92e26309beecca9ad370f85153e8f6d3d8f
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-01-03 13:15:21 +0900

    「A. 遅延評価で遅延リストを...」: サンプル追加

---
+++ samples/A_lazy_list/church.hs
@@ -0,0 +1,23 @@
+import Prelude hiding (fst, snd)
+
+data C a = C { (.$.) :: C a -> C a } | V { value :: a } | Nil
+
+instance Show a => Show (C a) where
+	show (C _) = "<function>"
+	show (V x) = "V " ++ show x
+	show Nil = "Nil"
+
+instance Num a => Num (C a) where
+	V x + V y = V x + V y
+	V x * V y = V x * V y
+	negate = V . negate . value
+	abs = V . abs . value
+	signum = V . signum . value
+	fromInteger = V . fromInteger
+
+pair :: C a -> C a -> C a
+pair x y = C $ \f -> f .$. x .$. y
+
+fst, snd :: C a -> C a
+fst p = p .$. C (\x -> C (\_ -> x))
+snd p = p .$. C (\_ -> C (\y -> y))
